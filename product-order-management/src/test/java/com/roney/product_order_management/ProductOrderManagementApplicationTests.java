e834c23d1e980333b1a416877fdfdbf868e7b15d
roney <roneydhara@gmail.com>
2025-10-01 22:10:17 +0530

    code first commit

---
+++ product-order-management/src/test/java/com/roney/product_order_management/ProductOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roney.product_order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
